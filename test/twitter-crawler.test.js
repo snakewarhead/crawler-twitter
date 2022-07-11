31eba29c1fdaba522d32388038819183f3f4d38d
snakewarhead <snakewarhead@gmail.com>
2022-07-11 23:34:52 +0800

    Update twitter-crawler.test.js

--- test/twitter-crawler.test.js
+++ test/twitter-crawler.test.js
@@ -1,5 +1,6 @@
 const Crawler = require('crawler')
-const Agent = require('socks5-https-client/lib/Agent')
+
+const PROXY = 'http://127.0.0.1:1081'
 
 describe('twitter crawler', () => {
   let crawler
@@ -7,23 +8,44 @@ describe('twitter crawler', () => {
     crawler = new Crawler({
       rateLimit: 3000,
       maxConnections: 1,
-      agentClass: Agent,
-      agentOptions: {
-        socksHost: '127.0.0.1',
-        socksPort: 1080,
+      userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:102.0) Gecko/20100101 Firefox/102.0',
+    })
+  })
+
+  it.skip('crawl google', (done) => {
+    const URL = 'https://www.google.com/'
+    crawler.direct({
+      uri: URL,
+      proxy: PROXY,
+      callback: (error, res) => {
+        if (error) {
+          console.error(error)
+          return
+        }
+
+        console.log(res.statusCode, '-------------')
+        console.log(res.$('#SIvCob > a:nth-child(1)').text())
+
+        done()
       },
     })
   })
 
-  it('crawl', (done) => {
-    crawler.direct({
-      uri: 'https://twitter.com/elonmusk',
-      callback: (error, response) => {
+  it('crawl twitter', (done) => {
+    const URL = 'https://twitter.com/elonmusk'
+    crawler.queue({
+      uri: URL,
+      proxy: PROXY,
+      callback: (error, res) => {
         if (error) {
-          console.log(error)
+          console.error(error)
           return
         }
-        console.log(response)
+
+        console.log(res.statusCode, '-------------')
+        console.log(res.body) // JavaScript is not available.
+        console.log('content', res.$('#id__9nfs1ilbodo > span:nth-child(1)').text())
+
         done()
       },
     })
