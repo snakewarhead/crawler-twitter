25b5067d19e072e33118c336d827404f8a1e23f2
lin <lin@192.168.1.102>
2022-07-18 12:07:37 +0800

    disable node-crawler

--- test/twitter-crawler.test.js
+++ test/twitter-crawler.test.js
@@ -1,53 +1,53 @@
-const Crawler = require('crawler')
+// const Crawler = require('crawler')
 
-const PROXY = 'http://127.0.0.1:1081'
+// const PROXY = 'http://127.0.0.1:1081'
 
-describe('twitter crawler', () => {
-  let crawler
-  before(() => {
-    crawler = new Crawler({
-      rateLimit: 3000,
-      maxConnections: 1,
-      userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:102.0) Gecko/20100101 Firefox/102.0',
-    })
-  })
+// describe('twitter crawler', () => {
+//   let crawler
+//   before(() => {
+//     crawler = new Crawler({
+//       rateLimit: 3000,
+//       maxConnections: 1,
+//       userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:102.0) Gecko/20100101 Firefox/102.0',
+//     })
+//   })
 
-  it.skip('crawl google', (done) => {
-    const URL = 'https://www.google.com/'
-    crawler.direct({
-      uri: URL,
-      proxy: PROXY,
-      callback: (error, res) => {
-        if (error) {
-          console.error(error)
-          return
-        }
+//   it.skip('crawl google', (done) => {
+//     const URL = 'https://www.google.com/'
+//     crawler.direct({
+//       uri: URL,
+//       proxy: PROXY,
+//       callback: (error, res) => {
+//         if (error) {
+//           console.error(error)
+//           return
+//         }
 
-        console.log(res.statusCode, '-------------')
-        console.log(res.$('#SIvCob > a:nth-child(1)').text())
+//         console.log(res.statusCode, '-------------')
+//         console.log(res.$('#SIvCob > a:nth-child(1)').text())
 
-        done()
-      },
-    })
-  })
+//         done()
+//       },
+//     })
+//   })
 
-  it('crawl twitter', (done) => {
-    const URL = 'https://twitter.com/elonmusk'
-    crawler.queue({
-      uri: URL,
-      proxy: PROXY,
-      callback: (error, res) => {
-        if (error) {
-          console.error(error)
-          return
-        }
+//   it('crawl twitter', (done) => {
+//     const URL = 'https://twitter.com/elonmusk'
+//     crawler.queue({
+//       uri: URL,
+//       proxy: PROXY,
+//       callback: (error, res) => {
+//         if (error) {
+//           console.error(error)
+//           return
+//         }
 
-        console.log(res.statusCode, '-------------')
-        console.log(res.body) // JavaScript is not available.
-        console.log('content', res.$('#id__9nfs1ilbodo > span:nth-child(1)').text())
+//         console.log(res.statusCode, '-------------')
+//         console.log(res.body) // JavaScript is not available.
+//         console.log('content', res.$('#id__9nfs1ilbodo > span:nth-child(1)').text())
 
-        done()
-      },
-    })
-  })
-})
+//         done()
+//       },
+//     })
+//   })
+// })
